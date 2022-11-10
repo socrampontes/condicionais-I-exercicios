6e836a02700fd4e6faebea4952abdee8bae54cfc
socrampontes <114445301+socrampontes@users.noreply.github.com>
2022-11-10 17:43:05 -0300

    exercicio condicionais

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,11 @@
-const num = //seu numero aqui
+/* # Exercício 1
 
-if(){
-    console.log("sua mensagem aqui");
+Crie uma função que receba uma const numérica qualquer. Crie um `if` para verificar se o número guardado na const é **par**. Caso seja, a função deve retornar a mensagem: "o número é par". Após imprima o retorno no console. */
+
+const num = 5
+
+if(num%2===0){
+    console.log("numero é par");
+}else{
+console.log("numero não é par")
 }
